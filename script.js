bdffa64ef27f80d7897f9d942b25f30ce78cc42f
Adrian157700 <156136100+Adrian157700@users.noreply.github.com>
2024-06-18 21:51:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const hour = document.querySelector('.hour');
-const minute = document.querySelector('.minutes');
-const second = document.querySelector('.seconds');
-const date = document.querySelector('.date');
+const hour = $('.hour');
+const minute = $('.minutes');
+const second = $('.seconds');
+const date = $('.date');
 const dateOptions = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
 
 const setValue = value => {
@@ -13,12 +13,43 @@ const setValue = value => {
 const setTime = () => {
   const current = new Date();
 
-  date.innerHTML = current.toLocaleDateString('pl', dateOptions);
-
-  hour.innerHTML = setValue(current.getHours());
-  minute.innerHTML = setValue(current.getMinutes());
-  second.innerHTML = setValue(current.getSeconds());
+  date.html(current.toLocaleDateString('pl', dateOptions));
+  hour.html(setValue(current.getHours()));
+  minute.html(setValue(current.getMinutes()));
+  second.html(setValue(current.getSeconds()));
 };
 
 setTime();
 setInterval(setTime, 1000);
+
+$(window).on('load', function() {
+    $('.container').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    const correctPassword = '1234'; 
+    $('#show-data-button').click(function() {
+        $('#password-modal').show();
+    });
+
+    $('.close').click(function() {
+        $('#password-modal').hide();
+    });
+
+    $('#submit-password').click(function() {
+        const enteredPassword = $('#password-input').val();
+        if (enteredPassword === correctPassword) {
+            $('#blurred-data').removeClass('blurred');
+            $('#password-modal').hide();
+            $('#show-data-button').fadeOut('slow');
+        } else {
+            alert('Niepoprawne hasło. Spróbuj ponownie.');
+        }
+    });
+
+    $(window).click(function(event) {
+        if (event.target.id === 'password-modal') {
+            $('#password-modal').hide();
+        }
+    });
+});
